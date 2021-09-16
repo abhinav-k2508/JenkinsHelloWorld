a3094019523d44a4ecf90bc22ecafeece7082a96
abhinav-k2508 <55973698+abhinav-k2508@users.noreply.github.com>
2021-09-16 14:31:14 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[]args){
-		System.out.println("-----*****Hello This is Webhook-----*****");
+		System.out.println("------*****Hello This is Webhook------*****");
 	}
 }
